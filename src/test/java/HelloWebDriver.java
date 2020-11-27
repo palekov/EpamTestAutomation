97c86437adea04e3661897da97e00fd9a2718097
Alexander <palekov-2011@mail.ru>
2020-11-27 22:04:10 +0300

    hello webdriver

---
+++ src/test/java/HelloWebDriver.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWebDriver {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://seleniumhq.org");
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+}
